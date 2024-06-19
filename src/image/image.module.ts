969873c25ca45e87a4ef1d4dca2174e0d5189ec4
LesHolikov <sanyagolikov97@gmoil.com>
2024-06-19 22:00:35 +0300

    Save img on localmachine

--- src/image/image.module.ts
+++ src/image/image.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
-import { ImageController } from './image.controller';
 import { MulterModule } from '@nestjs/platform-express';
+import * as os from 'os';
+import * as path from 'path';
+import { ImageController } from './image.controller';
+import * as multer from 'multer';
 
 @Module({
-  imports: [MulterModule.register({ dest: './uploads' })],
+  imports: [
+    MulterModule.registerAsync({
+      useFactory: () => ({
+        storage: multer.diskStorage({
+          destination: (req, file, cb) => {
+            cb(null, path.join(os.homedir(), 'Desktop'));
+          },
+          filename: (req, file, cb) => {
+            cb(null, file.originalname);
+          },
+        }),
+      }),
+    }),
+  ],
   controllers: [ImageController],
 })
 export class ImageModule {}
